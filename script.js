5291e3f84556a6ea4c278b3fc782bf4bd5f18826
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-05-17 23:21:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+let value = parseInt(prompt('Enter a number'))
+
+function factorial(value) {
+	let fact = 1;
+	for(let i=1;i<=value;i++){
+	fact = fact * i;
+	}
+	alert(`The factorial of ${value} is ${fact}`);
+} 
+
+factorial(value);
+
